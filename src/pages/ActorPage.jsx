3fc6dc4c531fed7d9fb910c296ca70a64c166cad
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-11-19 18:04:08 +0200

    move info card to its own component

--- src/pages/ActorPage.jsx
+++ src/pages/ActorPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ActorInfoCard from "../components/ActorInfoCard";
 import {
   fetchExternalLinks,
   fetchPerson,
@@ -17,49 +18,11 @@ export default function ActorPage() {
     fetchExternalLinks("person", id, setExternalLinks);
   }, []);
 
-  const getInfoValueJsx = ({ title, value }) => {
-    if (title == "gender")
-      return value == 0 ? (
-        <p>other</p>
-      ) : value == 1 ? (
-        <p>female</p>
-      ) : (
-        <p>male</p>
-      );
-    if (title == "also known as") return value.map((i) => <p>{i}</p>);
-    return <p>{value}</p>;
-  };
-
   if (!person) return <div>loading</div>;
   return (
     <div className="actor-page">
-      <div className="card-column">
-        <img
-          src={imgUrl + person.profile_path}
-          alt={person.name}
-          className="actor-img"
-        />
-        <div className="external-links">
-          {externalLinks.map((item) => (
-            <a href={item.link}>
-              <img
-                src={require(`../assets/icons/${item.title}.png`)}
-                alt=""
-                className="icon"
-              />
-            </a>
-          ))}
-        </div>
-        <div className="info">
-          <h1>Personal Info</h1>
-          {person.info.map((item) => (
-            <React.Fragment>
-              <h2>{item.title}</h2>
-              {getInfoValueJsx(item)}
-            </React.Fragment>
-          ))}
-        </div>
-      </div>
+      <ActorInfoCard person={person} externalLinks={externalLinks} />
+
       <div className="overview-column"></div>
     </div>
   );
