70fba681f6cb2d7f011fe93c4fd9f02837d1ea99
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2022-11-24 14:57:58 +0200

    convert the sliders to pure-react-slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,6 +6,14 @@ import "../css/slider.css";
 import { Link } from "react-router-dom";
 import { ColorRing } from "react-loader-spinner";
 import SliderCard from "./SliderCard";
+import {
+  CarouselProvider,
+  Slider as CarouselSlider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 export default function Slider({ listType, id, listName }) {
   const [list, setList] = useState([]);
@@ -16,7 +24,27 @@ export default function Slider({ listType, id, listName }) {
       } else fetchList(listType, listName, setList);
     }, 0);
   }, []);
-
+  return (
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={125}
+      className="slider"
+      totalSlides={list.length}
+      visibleSlides={7}
+    >
+      <CarouselSlider>
+        {list.map((item, index) => (
+          <Slide index={index}>
+            <SliderCard
+              listType={listType == "person" ? item.media_type : listType}
+              index={index}
+              item={item}
+            />
+          </Slide>
+        ))}
+      </CarouselSlider>
+    </CarouselProvider>
+  );
   return (
     <div className="slider">
       {list.map((item, index) => (
